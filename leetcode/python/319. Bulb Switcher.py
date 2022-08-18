b7c1902ab1bd2d9f6d8bf48c892d676fee9a10f4
Ruddy <rudranilshil@gmail.com>
2022-08-18 20:45:50 +0530

    18.8.22

---
+++ leetcode/python/319. Bulb Switcher.py
@@ -0,0 +1,27 @@
+class Solution:
+    # def bulbSwitch(self, n: int) -> int:
+    def bulbSwitch(n):
+        return int(n**(1/2))
+    print(bulbSwitch(8))
+
+
+# here we use double asterisk[**] this double asterisk are a kind of Arithemetic operators
+# also knows as power operators.
+
+# In a function definition, the double asterisk is also known ** kwargs
+
+# example:
+
+# a = 2
+# b = 5
+# c = a**b
+# print(c)
+
+# output:
+# 32
+
+# calculation:
+# 2⁵ = 2×2×2×2×2 = 32
+
+# basically ** calculates the exponent where,
+# the first variable is the base and 2nd variable is the exponent variable
