0e4e5a78bb1b176b19f8bb9503b2b461f4efed15
Ruddy <rudranilshil@gmail.com>
2022-11-07 01:05:26 +0530

    binary search folder made and base program added

---
+++ for_notes/binary search/binary search using itteration.py
@@ -0,0 +1,22 @@
+def binarysearch(arr, f, l, n):
+    # f=first index
+    # l= last index
+    while l-f > 1:
+        mid = (f+l)//2
+        if arr[mid] < n:
+            f = mid+1
+        else:
+            l = mid
+    if arr[f] == n:
+        print(f)
+    elif arr[l] == n:
+        print(l)
+    else:
+        print('not found')
+
+
+arr = [1, 2, 6, 7, 8, 9]
+n = 6
+f = 0
+l = len(arr)-1
+binarysearch(arr, f, l, n)
