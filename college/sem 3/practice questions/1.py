4ca9792f56089fc1d2652e2ef07cb0fb448b7b9b
Ruddy <rudranilshil@gmail.com>
2023-02-01 01:33:35 +0530

    practice code

---
+++ college/sem 3/practice questions/1.py
@@ -0,0 +1,8 @@
+def product(a, b):
+    if (a*b > 1000):
+        return a+b
+    else:
+        return a*b
+
+
+print(product(10, 20))
---
+++ college/sem 3/practice questions/2.py
@@ -0,0 +1,8 @@
+def arr(arr):
+    for i in arr:
+        if i % 5 == 0 and i % 3 == 0:
+            print(i)
+
+
+arry = [4, 15, 12, 45]
+arr(arry)
